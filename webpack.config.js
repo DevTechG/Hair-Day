c93a7b0f71f72d59eb10be4797a701dc7c3c5ee3
DevTechG <gabriel10300007@gmail.com>
2025-02-07 20:05:57 -0300

    Configuring the CSS module and plugin to copy “assets” to “dist”.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const HTMLWebpack = require("html-webpack-plugin")
+const copyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
     target: "web",
@@ -24,7 +25,25 @@ module.exports = {
 
     plugins: [
         new HTMLWebpack({
-            template: path.resolve(__dirname, "index.html")
+            template: path.resolve(__dirname, "index.html"),
+            favicon: path.resolve("src","assets","scissors.svg")
+        }),
+        new copyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets"),
+                    to: path.resolve(__dirname, "dist", "src", "assets")
+                }
+            ]
         })
-    ]
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+        ],
+    },
 }
